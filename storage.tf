4a3afd7be644402af7e1c92089fd29e174c27f91
SebastianHernandez69 <sh3641046@gmail.com>
2025-04-03 12:58:04 -0600

    feat: creacion de infraestructura para almacenamiento de archivos y cola de mensajeria

---
+++ storage.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "sa" {
+    name = "sa${var.project}${var.environment}exp0700"
+    resource_group_name = azurerm_resource_group.ec-rg.name
+    location = var.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+
+    tags = var.tags
+}
+
+resource "azurerm_storage_container" "ca"{
+    name = "files"
+    container_access_type = "private"
+    storage_account_id = azurerm_storage_account.sa.id
+}
+
+resource "azurerm_storage_queue" "qm"{
+    name = "messagingsystem"
+    storage_account_name = azurerm_storage_account.sa.name
+}
